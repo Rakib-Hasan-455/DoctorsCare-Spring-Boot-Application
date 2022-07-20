2b5071aaa89fc91c54a4454bc2fa42dcb4f928fe
Rakib-Hasan-455 <hasanrakib455@gmail.com>
2022-07-21 01:04:08 +0600

    Version 2.2 Updated Till Patient Image Upload & DP

--- src/main/java/com/example/doctorscarespringbootapplication/configuration/fileUploadHelper/FileUploadHelper.java
+++ src/main/java/com/example/doctorscarespringbootapplication/configuration/fileUploadHelper/FileUploadHelper.java
@@ -1,5 +1,6 @@
 package com.example.doctorscarespringbootapplication.configuration.fileUploadHelper;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -8,21 +9,23 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 @Service
 @Component
 public class FileUploadHelper {
-    // uploadDir = "G:\\" -> Static path
-    public final String UploadDir = new ClassPathResource("/static/images/userUploadedImages").getFile().getAbsolutePath();
+    private String uploadDir = "upload";
 
     public FileUploadHelper() throws IOException {
+        var uploadPath = Paths.get(uploadDir);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectory(uploadPath);
+        }
     }
 
     public String uploadImageFile(MultipartFile multipartFile, String imageName) throws IOException {
-        System.out.println(UploadDir);
-        System.out.println(UploadDir+File.separator+imageName);
         MultipartFile file = multipartFile;
         System.out.println((double) (file.getSize()/(1024.0*1024.0))+" MB");
         System.out.println(file.getContentType());
@@ -30,8 +33,8 @@ public class FileUploadHelper {
         System.out.println(file.getOriginalFilename());
         String uploadFileImgDir = null;
          try {
-             Files.copy(multipartFile.getInputStream(), Paths.get(UploadDir+File.separator+imageName), StandardCopyOption.REPLACE_EXISTING);
-             uploadFileImgDir = UploadDir+File.separator+imageName;
+             Files.copy(multipartFile.getInputStream(), Paths.get(uploadDir+File.separator+imageName), StandardCopyOption.REPLACE_EXISTING);
+             uploadFileImgDir = uploadDir+File.separator+imageName;
              return uploadFileImgDir;
          } catch (Exception e) {
              return uploadFileImgDir;
