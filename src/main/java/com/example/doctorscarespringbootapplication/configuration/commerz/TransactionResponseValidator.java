8baacc97c6fa093168f3bfde7448a8f0da34558a
Rakib-Hasan-455 <hasanrakib455@gmail.com>
2022-08-09 23:39:55 +0600

    Version 3.5 Payment With SSL Wireless Sandbox Pay Test Phase 5

--- src/main/java/com/example/doctorscarespringbootapplication/configuration/commerz/TransactionResponseValidator.java
+++ src/main/java/com/example/doctorscarespringbootapplication/configuration/commerz/TransactionResponseValidator.java
@@ -19,17 +19,17 @@ public class TransactionResponseValidator {
      * @throws Exception
      * Send Received params from your success resoponse (POST ) in this Map</>
      */
-    @Autowired
-    private AppointDoctorTransactionRepository appointDoctorTransactionRepository;
+//    @Autowired
+//    private AppointDoctorTransactionRepository appointDoctorTransactionRepository;
     public boolean receiveSuccessResponse(Map<String, String> request) throws Exception {
 
         String trxId = request.get("tran_id");
         /**
          *Get your AMOUNT and Currency FROM DB to initiate this Transaction
          */
-        AppointDoctorTransaction appointDoctorTransaction = appointDoctorTransactionRepository.findByTxid(trxId);
-        String amount = appointDoctorTransaction.getDoctorFee();
-//        String amount = "150";
+//        AppointDoctorTransaction appointDoctorTransaction = appointDoctorTransactionRepository.findByTxid(trxId);
+//        String amount = appointDoctorTransaction.getDoctorFee();
+        String amount = "150";
         String currency = "BDT";
         // Set your store Id and store password and define TestMode
         SSLCommerz sslcz = new SSLCommerz("docto62f28257d4314", "docto62f28257d4314@ssl", true);
